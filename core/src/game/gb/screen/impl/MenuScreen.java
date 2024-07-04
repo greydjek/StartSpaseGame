45f4b4172e66973b24620b40eae19462fd5d5828
Админ <delux26rus@mail.ru>
2024-07-04 09:14:12 +0300

    движение за курсором

--- core/src/game/gb/screen/impl/MenuScreen.java
+++ core/src/game/gb/screen/impl/MenuScreen.java
@@ -23,19 +23,27 @@ private Vector2 v;
     v= new Vector2(1,1);
     adnVector2 = new Vector2();
     }
+
 Vector2 retVector(Vector2 v, Vector2 v2){
-        if(v.x!=v2.x & v.x<v2.x){
-            v.add(1,0);
-            if(v.x!=v2.x & v.x>v2.x){
-                v.add(-1,0);
-            }
-            if(v.y!= v2.y & v.y<v2.y){
-                v.add(0,1);
-                if(v.y!= v2.y & v.y>v2.y){
-                    v.add(0,-1);
-                }
-            }
-        }
+  //   while((v.x != v2.x)&& (v.y != v2.y) ) {
+   if ((v.x != v2.x) | (v.y != v2.y)){
+       if (v.x < v2.x) {
+           v.add(1, 0);
+       }
+       //     if (v.x != v2.x)
+       if (v.x > v2.x) {
+           v.add(-1, 0);
+       }
+     //         if (v.y != v2.y)
+       if (v.y < v2.y) {
+           v.add(0, 1);
+       }
+   //                if (v.y != v2.y)
+       if (v.y > v2.y) {
+           v.add(0, -1);
+       }
+
+   }
     return v;
     }
     @Override
@@ -44,9 +52,8 @@ Vector2 retVector(Vector2 v, Vector2 v2){
 batch.begin();
 batch.draw(img,start.x,start.y);
 batch.end();
-if(!touch.equals(adnVector2)){
-touch.add(retVector(start,adnVector2));
-if( )
+if(!start.equals(adnVector2)){
+ start.set(retVector(start,adnVector2));
 }
 
 //touch.add(v);// прибавляем вектор получаем Эффект движения (чем больше вектор тем быстрее)
